31d282fbecaea77726ef711ea074e4cfede4eb6a
Kathy Lu <kaffy.lu@gmail.com>
2016-01-06 21:14:12 -0500

    changed beforeinstall events to afterinstall

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,1 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
-npm install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
+npm install
 grunt init
 grunt prod
 NODE_ENV=production forever start index.js
